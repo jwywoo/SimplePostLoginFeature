efddf899f1805394fd4cf865fb9da795c7b8b376
jwywoo <jwywoo26@gmail.com>
2023-09-02 13:46:43 +0900

    First Commit: JWYWOO: Project dependencies added and setting done.

---
+++ src/main/java/com/example/simplepostloginfeature/SimplePostLoginFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplepostloginfeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePostLoginFeatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePostLoginFeatureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplepostloginfeature/SimplePostLoginFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplepostloginfeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePostLoginFeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
