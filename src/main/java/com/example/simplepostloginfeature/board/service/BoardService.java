e17c59179f87056831e5a1116bfe78ffd3272630
jwywoo <jwywoo26@gmail.com>
2023-09-03 21:45:51 +0900

    5th Commit: JWYWOO: BoardService Skeleton done

---
+++ src/main/java/com/example/simplepostloginfeature/board/service/BoardService.java
@@ -0,0 +1,42 @@
+package com.example.simplepostloginfeature.board.service;
+
+import com.example.simplepostloginfeature.board.dto.BoardResponseDto;
+import com.example.simplepostloginfeature.board.entity.Board;
+import com.example.simplepostloginfeature.board.repository.BoardRepository;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+public class BoardService {
+    private final BoardRepository boardRepository;
+    public BoardService(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
+    // Find all
+    public List<BoardResponseDto> findAllBoards() {
+        return null;
+    }
+    // Find one
+    public BoardResponseDto boardDetail(Long id) {
+        return null;
+    }
+    // Create -> authorization required (Request Header)
+    public BoardResponseDto boardCreate(Long id) {
+        return null;
+    }
+    // Update -> authorization required (Request Header)
+    @Transactional
+    public BoardResponseDto boardUpdate(Long id) {
+        return null;
+    }
+    // Delete -> authorization required (Request Header)
+    public BoardResponseDto boardDelete(Long id) {
+        return null;
+    }
+
+    // Find board by id
+    private Board findBoard(Long id) {
+        return null;
+    }
+}
